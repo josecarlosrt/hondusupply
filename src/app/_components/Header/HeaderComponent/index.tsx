3d2c99840ce41b6622d90da2ea7f191e730f795e
josecarlosrt <59973113+josecarlosrt@users.noreply.github.com>
2024-01-08 23:54:45 -0600

    Organized Header Nav Component

---
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Link from 'next/link'
+
+import { Header } from '../../../../payload/payload-types'
+import { Gutter } from '../../Gutter'
+
+import classes from './index.module.scss'
+import Image from 'next/image'
+import { HeaderNav } from '../Nav'
+import MobileNav from '../MobileNav'
+
+
+const HeaderComponent = ({ header }: {header: Header}) => {
+  return (
+    <nav className={[classes.header]}>
+        <Gutter className={classes.wrap}>
+            <Link href="/">
+            <Image src="/logo-black.svg" alt="logo" width={170} height={50}/>
+            </Link>
+
+            <HeaderNav header={header}/>
+            <MobileNav header={header}/>
+        </Gutter>
+    </nav>
+  )
+}
+
+export default HeaderComponent
